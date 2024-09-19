9e3f6f7528e40a9dcc1cf94befb71c69985c8136
Diego ROman <diegoroman108@gmail.com>
2024-09-19 15:42:02 -0300

    first commit

---
+++ main.js
@@ -0,0 +1,12 @@
+let caixa = prompt("Dos grandes,qual o time mais bonito do rio?")
+
+
+if (caixa === "") {
+  alert('Digite o nome do time');
+} else if (caixa === "Vasco" || caixa === "Vasco da gama") {
+  alert('Melhor escolha de todas');
+}else if (caixa === "Fluminense" || caixa === "Botafogo"){
+  alert('Ta ok');
+  }else if ( caixa =="Flamengo"){
+    alert ('KKKK que horrível ')
+  };
